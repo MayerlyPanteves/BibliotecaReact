e92d052cb1074707eda2c9fd63e1ae10731a3ffd
MAYERLY <mayerly_panteves@soy.sena.edu.co>
2025-07-07 23:07:49 -0500

    Ajuste application

--- src/main/java/edu/sena/bibliotecaspring/BibliotecaSpringApplication.java
+++ src/main/java/edu/sena/bibliotecaspring/BibliotecaSpringApplication.java
@@ -2,8 +2,10 @@ package edu.sena.bibliotecaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "edu.sena.bibliotecaspring")
 public class BibliotecaSpringApplication {
 
     public static void main(String[] args) {
