763c6dd0a2e0b6a314f79a6ad200d00f66647510
MAYERLY <mayerly_panteves@soy.sena.edu.co>
2025-07-14 20:59:24 -0500

    Ajustes inicio

---
+++ frontend-react/src/components/Navbar.js
@@ -0,0 +1,16 @@
+// src/components/Navbar.js
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Libros</li>
+                <li>Préstamos</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
